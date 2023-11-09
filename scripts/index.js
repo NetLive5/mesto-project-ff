4280a470515e59f9b77ab6485e0a984b8df0ea28
Netlive5 <sas.1800@mail.ru>
2023-11-09 17:18:34 +0300

    function add Card from profile & create function delete card

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
-
-// @todo: DOM узлы
+const cardList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+const addCard = (srcCard, titleCard) => {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = srcCard;
+  cardElement.querySelector(".card__title").textContent = titleCard;
+
+  cardList.append(cardElement);
+
+  const resetButton = cardElement.querySelector(".card__delete-button");
+  resetButton.addEventListener("click", deleteCard);
+};
 
 // @todo: Функция удаления карточки
+const deleteCard = (event) => {
+  const item = event.target.closest(".card");
+  item.remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (item) {
+  addCard(item.link, item.name);
+});
