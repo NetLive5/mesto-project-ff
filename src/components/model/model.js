34542b64c5c7943fe38c112a2a93ad7a2a2d8311
Igor Semenov <igor.nevski.25@gmail.com>
2024-02-13 13:51:46 +0300

    fix config

--- src/components/model/model.js
+++ src/components/model/model.js
@@ -1,36 +1,41 @@
 // закрытия модального окна
-const closeModalWindow = (value) => {
-  value.classList.remove("popup_is-opened", "popup_is-animated"); // Убираем классы, отвечающие за открытость и анимацию модального окна
-  document.removeEventListener("keydown", keyHandlerEsc); // Удаляем обработчик события для клавиши Esc
-  value.removeEventListener("click", closePopupByClick); // Удаляем обработчик события для клика по области модального окна или кнопке закрытия
+const closeModalWindow = value => {
+	value.classList.remove('popup_is-opened'); // Убираем классы, отвечающие за открытость и анимацию модального окна
+	document.removeEventListener('keydown', keyHandlerEsc); // Удаляем обработчик события для клавиши Esc
+	value.removeEventListener('click', closePopupByClick); // Удаляем обработчик события для клика по области модального окна или кнопке закрытия
 };
 
 // открытия модального окна
-const openModalWindow = (value) => {
-  value.classList.add("popup_is-opened", "popup_is-animated"); // Добавляем классы, что бы открыть модальное окно
-  document.addEventListener("keydown", keyHandlerEsc); // Добавляем обработчик события для клавиши Esc
-  value.addEventListener("click", closePopupByClick); // Добавляем обработчик события для клика по области
+const openModalWindow = value => {
+	value.classList.add('popup_is-animated');
+
+	setTimeout(() => {
+		value.classList.add('popup_is-opened');
+	}, 100); // Добавляем классы, что бы открыть модальное окно
+
+	document.addEventListener('keydown', keyHandlerEsc); // Добавляем обработчик события для клавиши Esc
+	value.addEventListener('click', closePopupByClick); // Добавляем обработчик события для клика по области
 };
 
 // модального окна по клику
-const closePopupByClick = (evt) => {
-  if (
-    evt.target === evt.currentTarget ||
-    evt.target.classList.contains("popup__close") // Проверяем, был ли клик по области модального окна или по кнопке закрытия
-  ) {
-    closeModalWindow(evt.currentTarget); // Закрываем модальное окно
-  }
+const closePopupByClick = evt => {
+	if (
+		evt.target === evt.currentTarget ||
+		evt.target.classList.contains('popup__close') // Проверяем, был ли клик по области модального окна или по кнопке закрытия
+	) {
+		closeModalWindow(evt.currentTarget); // Закрываем модальное окно
+	}
 };
 
 // модального окна на Esc
-const keyHandlerEsc = (event) => {
-  // Проверяем, была ли нажата клавиша Esc
-  if (event.key === "Escape") {
-    closeModalWindow(document.querySelector(".popup_is-opened"));
-  }
+const keyHandlerEsc = event => {
+	// Проверяем, была ли нажата клавиша Esc
+	if (event.key === 'Escape') {
+		closeModalWindow(document.querySelector('.popup_is-opened'));
+	}
 };
 
-export { openModalWindow, closeModalWindow };
+export { closeModalWindow, openModalWindow };
 
 //currentTarget - это свойство объекта события в JavaScript, которое указывает на элемент, на котором был установлен обработчик события.
 //evt.currentTarget — элемент, где сработал обработчик;
--- src/components/validation/validation.js
+++ src/components/validation/validation.js
@@ -3,124 +3,124 @@
 //---------------------------------------------------------------------------------------------------------------------------------------------------//
 
 import {
-  isValidURL,
-  isValidInputCharacters,
-  isValidImageURL,
-} from "./validation-regex"; // Импорт функция с регулярными выражениями
+	isValidImageURL,
+	isValidInputCharacters,
+	isValidURL,
+} from './validation-regex'; // Импорт функция с регулярными выражениями
 
 // Функция отображения сообщения об ошибке для конкретного поля ввода
 const showInputError = (
-  formElement,
-  inputElement,
-  errorMessage,
-  validationConfig
+	formElement,
+	inputElement,
+	errorMessage,
+	validationConfig
 ) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`); // Создаем константу, предназначенную для отображения ошибок валидации для конкретного поля
-  inputElement.classList.add(validationConfig.inputErrorClass); // Добавляем стили ошибки
-  errorElement.textContent = errorMessage; // Задаем текст сообщения об ошибке
-  errorElement.classList.add(validationConfig.errorClass); // Добавляем стили для активации сообщения об ошибке
+	const errorElement = formElement.querySelector(`.${inputElement.id}-error`); // Создаем константу, предназначенную для отображения ошибок валидации для конкретного поля
+	inputElement.classList.add(validationConfig.inputErrorClass); // Добавляем стили ошибки
+	errorElement.textContent = errorMessage; // Задаем текст сообщения об ошибке
+	errorElement.classList.add(validationConfig.errorClass); // Добавляем стили для активации сообщения об ошибке
 };
 
 // Функция скрытия сообщения об ошибке для конкретного поля ввода
 const hideInputError = (formElement, inputElement, validationConfig) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove(validationConfig.inputErrorClass); // Удаляем стили ошибки
-  errorElement.classList.remove(validationConfig.errorClass); // Удаляем стили для активации сообщения об ошибке
-  errorElement.textContent = ""; // Очищаем текст сообщения об ошибке
+	const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+	inputElement.classList.remove(validationConfig.inputErrorClass); // Удаляем стили ошибки
+	errorElement.classList.remove(validationConfig.errorClass); // Удаляем стили для активации сообщения об ошибке
+	errorElement.textContent = ''; // Очищаем текст сообщения об ошибке
 };
 
 // Функция проверки валидности конкретного поля ввода
 const checkInputValidity = (formElement, inputElement, validationConfig) => {
-  const errorMessage =
-    "Разрешены только латинские, кириллические буквы, знаки дефиса и пробелы";
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, errorMessage, validationConfig); // Показываем сообщение об ошибке браузера
-  } else if (inputElement.type === "url" && !isValidURL(inputElement.value)) {
-    showInputError(
-      formElement,
-      inputElement,
-      "Введите корректный URL",
-      validationConfig
-    ); // Показываем сообщение об ошибке для URL
-  } else if (
-    inputElement.name === "link" &&
-    !isValidImageURL(inputElement.value)
-  ) {
-    showInputError(
-      formElement,
-      inputElement,
-      "Введите корректный URL изображения",
-      validationConfig
-    ); // Показываем сообщение об ошибке для URL изображения
-  } else if (!isValidInputCharacters(inputElement)) {
-    showInputError(formElement, inputElement, errorMessage, validationConfig); // Показываем сообщение об ошибке для недопустимых символов
-  } else {
-    hideInputError(formElement, inputElement, validationConfig); // Скрываем сообщение об ошибке, если все в порядке
-  }
+	const errorMessage =
+		'Разрешены только латинские, кириллические буквы, знаки дефиса и пробелы';
+	if (!inputElement.validity.valid) {
+		showInputError(formElement, inputElement, errorMessage, validationConfig); // Показываем сообщение об ошибке браузера
+	} else if (inputElement.type === 'url' && !isValidURL(inputElement.value)) {
+		showInputError(
+			formElement,
+			inputElement,
+			'Введите корректный URL',
+			validationConfig
+		); // Показываем сообщение об ошибке для URL
+	} else if (
+		inputElement.name === 'link' &&
+		!isValidImageURL(inputElement.value)
+	) {
+		showInputError(
+			formElement,
+			inputElement,
+			'Введите корректный URL изображения',
+			validationConfig
+		); // Показываем сообщение об ошибке для URL изображения
+	} else if (!isValidInputCharacters(inputElement)) {
+		showInputError(formElement, inputElement, errorMessage, validationConfig); // Показываем сообщение об ошибке для недопустимых символов
+	} else {
+		hideInputError(formElement, inputElement, validationConfig); // Скрываем сообщение об ошибке, если все в порядке
+	}
 };
 // Функция для проверки наличия хотя бы одного невалидного поля
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return (
-      !inputElement.validity.valid || !isValidInputCharacters(inputElement)
-    ); // Проверяем валидность поля и символы в нем
-  });
+const hasInvalidInput = inputList => {
+	return inputList.some(inputElement => {
+		return (
+			!inputElement.validity.valid || !isValidInputCharacters(inputElement)
+		); // Проверяем валидность поля и символы в нем
+	});
 };
 
 // Функция переключения состояния кнопки в зависимости от валидности полей ввода
 const toggleButtonState = (inputList, buttonElement, validationConfig) => {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.disabled = true; // Если хотя бы одно поле невалидно, делаем кнопку неактивной
-    buttonElement.classList.add(validationConfig.inactiveButtonClass);
-  } else {
-    buttonElement.disabled = false; // Если все поля валидны, делаем кнопку активной
-    buttonElement.classList.remove(validationConfig.inactiveButtonClass);
-  }
+	if (hasInvalidInput(inputList)) {
+		buttonElement.disabled = true; // Если хотя бы одно поле невалидно, делаем кнопку неактивной
+		buttonElement.classList.add(validationConfig.inactiveButtonClass);
+	} else {
+		buttonElement.disabled = false; // Если все поля валидны, делаем кнопку активной
+		buttonElement.classList.remove(validationConfig.inactiveButtonClass);
+	}
 };
 
 // Функция включения валидации для всех форм на странице
-const enableValidation = (validationConfig) => {
-  const formList = Array.from(
-    document.querySelectorAll(validationConfig.formSelector)
-  );
+const enableValidation = validationConfig => {
+	const formList = Array.from(
+		document.querySelectorAll(validationConfig.formSelector)
+	);
 
-  formList.forEach((formElement) => {
-    formElement.addEventListener("submit", (evt) => {
-      evt.preventDefault(); // Предотвращаем отправку формы по умолчанию
-    });
+	formList.forEach(formElement => {
+		formElement.addEventListener('submit', evt => {
+			evt.preventDefault(); // Предотвращаем отправку формы по умолчанию
+		});
 
-    const inputList = Array.from(
-      formElement.querySelectorAll(validationConfig.inputSelector)
-    );
-    const buttonElement = formElement.querySelector(
-      validationConfig.submitButtonSelector
-    );
+		const inputList = Array.from(
+			formElement.querySelectorAll(validationConfig.inputSelector)
+		);
+		const buttonElement = formElement.querySelector(
+			validationConfig.submitButtonSelector
+		);
 
-    toggleButtonState(inputList, buttonElement, validationConfig);
+		toggleButtonState(inputList, buttonElement, validationConfig);
 
-    inputList.forEach((inputElement) => {
-      inputElement.addEventListener("input", function () {
-        checkInputValidity(formElement, inputElement, validationConfig);
-        toggleButtonState(inputList, buttonElement);
-      });
-    });
-  });
+		inputList.forEach(inputElement => {
+			inputElement.addEventListener('input', function () {
+				checkInputValidity(formElement, inputElement, validationConfig);
+				toggleButtonState(inputList, buttonElement, validationConfig);
+			});
+		});
+	});
 };
 
 const clearValidation = (formElement, validationConfig) => {
-  const inputList = Array.from(
-    formElement.querySelectorAll(validationConfig.inputSelector)
-  );
-  const buttonElement = formElement.querySelector(
-    validationConfig.submitButtonSelector
-  );
+	const inputList = Array.from(
+		formElement.querySelectorAll(validationConfig.inputSelector)
+	);
+	const buttonElement = formElement.querySelector(
+		validationConfig.submitButtonSelector
+	);
 
-  inputList.forEach((inputElement) => {
-    hideInputError(formElement, inputElement, validationConfig);
-  });
+	inputList.forEach(inputElement => {
+		hideInputError(formElement, inputElement, validationConfig);
+	});
 
-  buttonElement.disabled = true;
-  buttonElement.classList.add(validationConfig.inactiveButtonClass);
+	buttonElement.disabled = true;
+	buttonElement.classList.add(validationConfig.inactiveButtonClass);
 };
 
-export { enableValidation, clearValidation };
+export { clearValidation, enableValidation };
